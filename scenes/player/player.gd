a11bd1a44c8b55ae2eb2df7344725a7890d7a6a0
Octoan1 <okloepping@gmail.com>
2025-10-10 09:57:54 -0500

    changed player script
    - changed the player script so that players can look directly vertical
    - also deleted extra player scene

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -15,10 +15,11 @@ func _unhandled_input(event: InputEvent) -> void:
 	elif event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		
 		if event is InputEventMouseMotion:
 			neck.rotate_y(-event.relative.x * 0.01)
 			camera.rotate_x(-event.relative.y * 0.01)
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 
 
 func _physics_process(delta: float) -> void:
