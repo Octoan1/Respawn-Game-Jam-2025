4a364566aabd58d254b0f40a28337d6a08ecefa2
Connradical <conneremoore@gmail.com>
2025-10-11 17:28:57 -0500

    Ring Puzzle Nearly Complete
    It has a solution  and a setup, and can be played. Just needs a bit more testing and polish to the ring rotations

--- scenes/Button.gd
+++ scenes/Button.gd
@@ -2,27 +2,33 @@ extends Node3D
 
 @export var button_name: String
 
-signal outerRotate()
-signal innerRotate()
+var off_cooldown = false
 
-func _ready():
-	# if this is in a different main scene thing, change "RingPuzzle" to "main" etc.
-	var emittor = get_node("/root/RingPuzzle/Player")
-	if emittor:
-		emittor.connect("area_entered", Callable(self, "button_pressed"))
-	
+signal OuterMiddleRotate()
+signal InnerOuterRotate()
 
 # this takes parameter from signal connected FROM player script.
 # player script detects bodys entering the area3D, and this script will make
 # sure only player activates a button
-
-func inner_button_pressed(body: Node3D) -> void:
+func inner_outer_button_pressed(body: Node3D) -> void:
 	if(body.name=="Player"):
-		print("Inner rotating now")
-		emit_signal("innerRotate")
+		if(off_cooldown == true):
+			print("Inner rotating now")
+			emit_signal("InnerOuterRotate")
+			off_cooldown = false
+			$ButtonCooldown.start()
 
 
-func outer_button_pressed(body: Node3D) -> void:
+func outer_middle_button_pressed(body: Node3D) -> void:
+	
 	if(body.name=="Player"):
-		print("Outer rotating now")
-		emit_signal("outerRotate")
+		if(off_cooldown == true):
+			print("Outer rotating now")
+			emit_signal("OuterMiddleRotate")
+			off_cooldown = false
+			$ButtonCooldown.start()
+
+
+func on_cooldown() -> void:
+	off_cooldown = true
+	print("Button cooldown refreshed!")
--- scenes/PuzzleRings.gd
+++ scenes/PuzzleRings.gd
@@ -8,21 +8,62 @@ var MiddleRing = $MiddleRing
 @onready
 var InnerRing = $InnerRing
 
+
+
 func _ready():
 	# if this is in a different main scene thing, change "RingPuzzle" to "main" etc.
 	var button = get_node("/root/RingPuzzle/Buttons")
 	if button:
-		button.connect("innerRotate", Callable(self, "inner_shift"))
-		button.connect("outerRotate", Callable(self, "outer_shift"))
+		button.connect("InnerOuterRotate", Callable(self, "inner_outer_shift"))
+		button.connect("OuterMiddleRotate", Callable(self, "outer_middle_shift"))
 
-func inner_shift() -> void:
-	print("Inner ring shifting now.")
-	print(InnerRing.get_state())
+func inner_outer_shift() -> void:
+	
 	InnerRing.set_state(InnerRing.get_state() + 1)
-	InnerRing.rotation()
-
-func outer_shift() -> void:
-	print("Outer ring shifting now.")
-	print(OuterRing.get_state())
 	OuterRing.set_state(OuterRing.get_state() + 1)
+	#purely visual for the player
+	InnerRing.rotation()
 	OuterRing.rotation()
+	
+	print("Outer state: ", OuterRing.get_state())
+	print("Middle state: ", MiddleRing.get_state())
+	print("Inner state: ", InnerRing.get_state())
+	
+	check_correct()
+	check_overflow()
+
+func outer_middle_shift() -> void:
+	
+	OuterRing.set_state(OuterRing.get_state() - 1)
+	MiddleRing.set_state(MiddleRing.get_state() + 1)
+	
+	#purely visual for the player
+	OuterRing.rotation()
+	MiddleRing.rotation()
+	
+	print("Outer state: ", OuterRing.get_state())
+	print("Middle state: ", MiddleRing.get_state())
+	print("Inner state: ", InnerRing.get_state())
+	
+	check_correct()
+	check_overflow()
+
+# Starting states are: outer;2  inner;3  middle;0
+#
+func check_correct() -> void:
+	var outerState = OuterRing.get_state()
+	var innerState = InnerRing.get_state()
+	var middleState = MiddleRing.get_state()
+	
+	if(outerState == 4 && middleState == 3 && innerState == 2):
+		print("Winner!")
+
+func check_overflow() -> void:
+	if(OuterRing.get_state() >= 5):
+		OuterRing.set_state(0)
+	elif(OuterRing.get_state() <= 0):
+		OuterRing.set_state(5)
+	if(MiddleRing.get_state() >= 5):
+		MiddleRing.set_state(0)
+	if(InnerRing.get_state() >= 5):
+		InnerRing.set_state(0)
