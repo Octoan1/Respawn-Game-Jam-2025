771ce8e3c774e6fca500a545c17568aebc0ae06f
Connradical <conneremoore@gmail.com>
2025-10-12 17:28:16 -0500

    Changed puzzle rings again
    Possible ring states: 1-5
    When ring is at state 5 and button is pressed, it overflows back to state 1.
    
    Each state is mapped to an angle of rotation: 1:72, 2:144, ..., 5:360
    
    Rings follow this rotation, but don't even work somehow?? Feel free to spend either 5 minutes or 5 hours fixing this, because it is probably easier than I'm making it.

--- scenes/PuzzleRings.gd
+++ scenes/PuzzleRings.gd
@@ -15,17 +15,21 @@ func _ready():
 	if button:
 		button.connect("InnerOuterRotate", Callable(self, "inner_outer_shift"))
 		button.connect("OuterMiddleRotate", Callable(self, "outer_middle_shift"))
+	
+	OuterRing.choose_rotation()
+	MiddleRing.choose_rotation()
+	InnerRing.choose_rotation()
 
 func inner_outer_shift() -> void:
-	if(InnerRing.get_state() != 4):
+	if(InnerRing.get_state() != 5):
 		InnerRing.set_state(InnerRing.get_state() + 1)
-	elif(InnerRing.get_state() == 4):
-		InnerRing.set_state(0)
+	elif(InnerRing.get_state() == 5):
+		InnerRing.set_state(1)
 	
-	if(OuterRing.get_state() != 4):
+	if(OuterRing.get_state() != 5):
 		OuterRing.set_state(OuterRing.get_state() + 1)
-	elif(OuterRing.get_state() == 4):
-		OuterRing.set_state(0)
+	elif(OuterRing.get_state() == 5):
+		OuterRing.set_state(1)
 	
 	#purely visual for the player
 	InnerRing.choose_rotation()
@@ -38,15 +42,15 @@ func inner_outer_shift() -> void:
 	print("Inner state: ", InnerRing.get_state())
 
 func outer_middle_shift() -> void:
-	if(MiddleRing.get_state() != 4):
+	if(MiddleRing.get_state() != 5):
 		MiddleRing.set_state(MiddleRing.get_state() + 1)
-	elif(MiddleRing.get_state() == 4):
-		MiddleRing.set_state(0)
+	elif(MiddleRing.get_state() == 5):
+		MiddleRing.set_state(1)
 	
-	if(OuterRing.get_state() != 4):
+	if(OuterRing.get_state() != 5):
 		OuterRing.set_state(OuterRing.get_state() + 1)
-	elif(OuterRing.get_state() == 4):
-		OuterRing.set_state(0)
+	elif(OuterRing.get_state() == 5):
+		OuterRing.set_state(1)
 	
 	#purely visual for the player
 	OuterRing.choose_rotation()
@@ -59,8 +63,9 @@ func outer_middle_shift() -> void:
 	print("Middle state: ", MiddleRing.get_state())
 	print("Inner state: ", InnerRing.get_state())
 
-# Starting states are: outer;2  inner;3  middle;0
-#
+# Starting states are: outer;2  middle;5 inner;3  
+# States range from 1-5. When it is at state 5, it resets to state 1.
+# (Resets from 360 degrees rotation to 72. So should be flush)
 func check_correct() -> void:
 	var outerState = OuterRing.get_state()
 	var innerState = InnerRing.get_state()
