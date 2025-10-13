cd2e332bf7f448f448306dfca398939435fcda41
JPrei20 <joeprei20@gmail.com>
2025-10-12 19:51:43 -0500

    Commit 3

--- scripts/GlobalTime.gd
+++ scripts/GlobalTime.gd
@@ -1,10 +1,15 @@
 extends Node
 
 # 0.0 -> 24.0 (hours)
-var time_of_day : float = 6.0  # Start at 6 AM
-var day_length : float = 60.0  # Seconds for a full day
+var time_of_day : float = 0.0  # Start at 6 AM
+var day_length : float = 0.25 * 60.0  # Seconds for a full day
+var day_counter : int = 0
+var stopped : bool = false
 
 func _process(delta: float) -> void:
 	# Increase time based on day length
-	time_of_day += (24.0 / day_length) * delta
-	time_of_day = fmod(time_of_day, 24.0)  # Wrap around 24 hours
+	if(!stopped):
+		time_of_day = fmod(time_of_day, 24.0)  # Wrap around 24 hours
+		time_of_day += (24.0 / day_length) * delta
+		if(time_of_day >= 24):
+			day_counter += 1
