9d2784e5f0a77de262102d1f2f3f7ffafd3c1750
Connradical <conneremoore@gmail.com>
2025-10-12 19:47:45 -0500

    Time-based Platforming Puzzle Completed
    Rough version is done. Functionally it is good. Not polished tho.

---
+++ scenes/environment/props/platform_rise.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+var t = GlobalTime.time_of_day 
+var speed = 0.01
+var start_pos = self.position.y
+
+func _physics_process(delta: float) -> void:
+	if t:
+		# stop it early so it doesn't go forever
+		if(self.position.y <= -16.5):
+			self.position.y = self.position.y + (t*delta*speed)
