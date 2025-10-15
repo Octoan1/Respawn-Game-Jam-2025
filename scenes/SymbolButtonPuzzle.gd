319d8a2ed0f3908d936d1b6b88eeff8c667cfa10
Connradical <conneremoore@gmail.com>
2025-10-15 00:54:03 -0500

    spawn puzzle complete
    - Spawn room and puzzle is complete
    - Jump puzzle is complete
    - All under "jump_puzzle" scene

---
+++ scenes/SymbolButtonPuzzle.gd
@@ -0,0 +1,147 @@
+extends Node3D
+var off_cooldown = false
+var solution = []
+var move_sun_button = false
+var move_moon_button = false
+var move_star_button = false
+var move_hourglass_button = false
+var speed = 0.3
+var wall_speed = 0.6
+var puzzle_complete = false
+var reset_button = false
+
+
+@onready
+var SunButton = $SunButton
+@onready
+var sun_start = SunButton.position.y
+@onready
+var MoonButton = $MoonButton
+@onready
+var moon_start = MoonButton.position.y
+@onready
+var StarButton = $StarButton
+@onready
+var star_start = StarButton.position.y
+@onready
+var HourglassButton = $HourglassButton
+@onready
+var hourglass_start = HourglassButton.position.y
+
+signal move_walls
+
+
+# this takes parameter from signal connected FROM player script.
+# player script detects bodys entering the area3D, and this script will make
+# sure only player activates a button
+func star_button_pressed(body: Node3D) -> void:
+	if(body.name=="Player"):
+		if(off_cooldown == true):
+			reset_button = false
+			move_star_button = true
+			solution.append("star")
+			
+			off_cooldown = false
+			$ButtonCooldown.start()
+			
+			solution_check()
+			reset_buttons()
+			
+
+
+func moon_button_pressed(body: Node3D) -> void:
+	print(body.name)
+	if(body.name=="Player"):
+		print("Player has entered!")
+		if(off_cooldown == true):
+			reset_button = false
+			move_moon_button = true
+			print("Moon button pressed")
+			solution.append("moon")
+			
+			# cooldown stuffs
+			off_cooldown = false
+			$ButtonCooldown.start()
+			
+			solution_check()
+			reset_buttons()
+
+func hourglass_button_pressed(body: Node3D) -> void:
+	if(body.name=="Player"):
+		if(off_cooldown == true):
+			reset_button = false
+			move_hourglass_button = true
+			solution.append("hourglass")
+			
+			off_cooldown = false
+			$ButtonCooldown.start()
+			
+			solution_check()
+			reset_buttons()
+
+func sun_button_pressed(body: Node3D) -> void:
+	if(body.name=="Player"):
+		if(off_cooldown == true):
+			reset_button = false
+			move_sun_button = true
+			solution.append("sun")
+			
+			off_cooldown = false
+			$ButtonCooldown.start()
+			
+			solution_check()
+			reset_buttons()
+			
+
+# Code: Sun moon star hourglass
+func solution_check() -> void:
+	print("Checking solution!")
+	for i in 4:
+		print(solution.get(i))
+	if(solution.get(0) == "sun" && solution.get(1) == "moon" && solution.get(2) == "star" && solution.get(3) == "hourglass"):
+		puzzle_complete = true
+		print("puzzle complete!")
+		emit_signal("move_walls")
+
+func reset_buttons() -> void:
+	if(move_sun_button == true && move_moon_button == true && move_hourglass_button == true && move_star_button == true && puzzle_complete == false):
+		move_sun_button = false
+		move_moon_button = false
+		move_hourglass_button = false
+		move_star_button = false
+		
+		
+		reset_button = true
+		solution = []
+
+func on_cooldown() -> void:
+	off_cooldown = true
+	print("Button cooldown refreshed!")
+
+
+func _process(delta: float) -> void:
+	if(move_sun_button == true):
+		if(SunButton.position.y >= sun_start-2.75):
+			SunButton.position.y = SunButton.position.y - delta*speed
+	
+	if(move_moon_button == true):
+		if(MoonButton.position.y >= moon_start-2.75):
+			MoonButton.position.y = MoonButton.position.y - delta*speed
+	
+	if(move_star_button == true):
+		if(StarButton.position.y >= star_start-2.75):
+			StarButton.position.y = StarButton.position.y - delta*speed
+	
+	if(move_hourglass_button == true):
+		if(HourglassButton.position.y >= hourglass_start-2.75):
+			HourglassButton.position.y = HourglassButton.position.y - delta*speed
+	
+	if(reset_button == true):
+		if(MoonButton.position.y <= moon_start):
+			MoonButton.position.y = MoonButton.position.y + delta*speed
+		if(SunButton.position.y <= sun_start):
+			SunButton.position.y = SunButton.position.y + delta*speed
+		if(StarButton.position.y <= star_start):
+			StarButton.position.y = StarButton.position.y + delta*speed
+		if(HourglassButton.position.y <= hourglass_start):
+			HourglassButton.position.y = HourglassButton.position.y + delta*speed
--- scenes/environment/props/platform_rise.gd
+++ scenes/environment/props/platform_rise.gd
@@ -1,16 +1,18 @@
 extends Node3D
 
-var t = GlobalTime.time_of_day 
-var speed = 0.01
+#var t = GlobalTime.time_of_day 
+var speed = 0.03
 @onready var start_pos = self.position.y
 
 
 func _process(delta: float) -> void:
-	print(t)
 	
+	var t = GlobalTime.time_of_day
 	if t:
-		# stop it early so it doesn't go forever
-		print(t)
-		if(self.position.y <= start_pos+5):
-			self.position.y = self.position.y + (t*delta*speed)
-			
+		if(t > 10 && t < 14):
+			# stop it early so it doesn't go forever
+			if(self.position.y <= start_pos+5):
+				self.position.y = self.position.y + (t*delta*speed)
+				
+		if(t > 14):
+			self.position.y = self.position.y - (t*delta*speed)
