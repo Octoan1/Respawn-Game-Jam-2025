884e932f2a6404d464598a56dbb829e9889b21a5
Connradical <conneremoore@gmail.com>
2025-10-15 18:17:44 -0500

    Fixes and New puzzle
    Jump, and spawn puzzle are all working as intended
    
    New puzzle is being worked on, with invisible  tiles
    
    decorations are being added

--- scenes/environment/props/platform_rise.gd
+++ scenes/environment/props/platform_rise.gd
@@ -9,10 +9,11 @@ func _process(delta: float) -> void:
 	
 	var t = GlobalTime.time_of_day
 	if t:
-		if(t > 10 && t < 14):
+		if(t > 10 && t < 20):
 			# stop it early so it doesn't go forever
 			if(self.position.y <= start_pos+5):
 				self.position.y = self.position.y + (t*delta*speed)
 				
-		if(t > 14):
-			self.position.y = self.position.y - (t*delta*speed)
+		elif(t > 20):
+			if(self.position.y >= start_pos):
+				self.position.y = self.position.y - (t*delta*speed)
