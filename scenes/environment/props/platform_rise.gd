ac35698faf727af9de61cf8f58af63df6bf14025
Connradical <conneremoore@gmail.com>
2025-10-14 21:40:26 -0500

    Just pushing to get from my computer
    yeah

--- scenes/environment/props/platform_rise.gd
+++ scenes/environment/props/platform_rise.gd
@@ -2,10 +2,15 @@ extends Node3D
 
 var t = GlobalTime.time_of_day 
 var speed = 0.01
-var start_pos = self.position.y
+@onready var start_pos = self.position.y
 
-func _physics_process(delta: float) -> void:
+
+func _process(delta: float) -> void:
+	print(t)
+	
 	if t:
 		# stop it early so it doesn't go forever
+		print(t)
 		if(self.position.y <= start_pos+5):
 			self.position.y = self.position.y + (t*delta*speed)
+			
