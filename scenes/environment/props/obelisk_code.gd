a7a48844028a1671d10d75172b1b082351572b78
JPrei20 <joeprei20@gmail.com>
2025-10-16 00:26:27 -0500

    Added Most of the Obelisk Puzzle

--- scenes/environment/props/obelisk_code.gd
+++ scenes/environment/props/obelisk_code.gd
@@ -2,11 +2,15 @@ extends Node3D
 
 @export var rise_time : int
 @export var sink_time : int
+var hidden_y = 0.0
+
+func _ready() -> void:
+	hidden_y = self.global_position.y
 
 func _process(delta: float) -> void:
 	var t = GlobalTime.time_of_day
 	if(t >= rise_time && t < sink_time):
-		if(self.global_position.y < -9):
-			self.global_position.y += delta * 0.5
-		elif(t >= sink_time && self.global_position.y > -14.32):
-			self.global_position.y -= delta * 0.5
+		if(self.global_position.y < hidden_y + 10):
+			self.global_position.y += delta * 2
+	elif(t >= sink_time && self.global_position.y > hidden_y):
+		self.global_position.y -= delta * 2
