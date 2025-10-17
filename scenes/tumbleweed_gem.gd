45357a53b4c2d87fed4e96e516332e1d23258254
JPrei20 <joeprei20@gmail.com>
2025-10-16 20:58:37 -0500

    Fixed Line Ups

--- scenes/tumbleweed_gem.gd
+++ scenes/tumbleweed_gem.gd
@@ -8,12 +8,12 @@ var gem : Node
 func _ready() -> void:
 	water_x = self.get_parent().get_child(0).global_position.x
 	water_z = self.get_parent().get_child(0).global_position.z
-	gem = self.get_parent().get_child(-1)
+	gem = self.get_parent().get_child(1)
 	gem.visible = false
 	
 func _process(_delta: float) -> void:
 	water_y = self.get_parent().get_child(0).global_position.y
-	gem = self.get_parent().get_child(-1)
+	gem = self.get_parent().get_child(1)
 	var distance = sqrt((self.get_child(0).global_position.x - water_x)**2 + (self.get_child(0).global_position.z - water_z)**2)
 	if(distance < 75 && self.get_child(0).global_position.y <= water_y + 23):
 		gem.global_position = self.get_child(0).global_position
