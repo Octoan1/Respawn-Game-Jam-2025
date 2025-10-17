3a552ed049b45c74eb426054ebba47ecb7cbda96
cobright-wisc <cobright@wisc.edu>
2025-10-16 21:07:28 -0500

    fixed spawn button error

--- scenes/SymbolButtonPuzzle.gd
+++ scenes/SymbolButtonPuzzle.gd
@@ -109,8 +109,6 @@ func sun_button_pressed(body: Node3D) -> void:
 # Code: Sun moon star hourglass
 func solution_check() -> void:
 	print("Checking solution!")
-	for i in 4:
-		print(solution.get(i))
 	if(solution.get(0) == "sun" && solution.get(1) == "moon" && solution.get(2) == "star" && solution.get(3) == "hourglass"):
 		puzzle_complete = true
 		print("puzzle complete!")
