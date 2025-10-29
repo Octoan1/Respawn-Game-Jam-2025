073ed2d4df87f0ddd338a395e80e4ebc170525b1
cobright-wisc <caloedu@pm.me>
2025-10-29 13:11:15 -0500

    Made things glowy

--- scenes/text.gd
+++ scenes/text.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	if material_override == null:
 		material_override = StandardMaterial3D.new()
 	material_override.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
-	material_override.albedo_color = Color(0,1,0,0)
+	material_override.albedo_color = Color(0.0, 2.844, 0.0, 0.0)
 	
 func _process(delta: float) -> void:
 	var t = GlobalTime.time_of_day
